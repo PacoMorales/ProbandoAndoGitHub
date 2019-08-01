c0610bc733514cef961bbdd9b12417243893474a
@PacoMoralesZarate <aprogramacion7@kreatsolutions.com.mx>
2019-08-01 10:41:23 -0500

    Primer controlador de prueba

---
+++ ProbandoAndoGitHub/Controllers/ProbandoAndoGitHubController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ProbandoAndoGitHub.Controllers
+{
+    public class ProbandoAndoGitHubController : Controller
+    {
+        // GET: ProbandoAndoGitHub
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
